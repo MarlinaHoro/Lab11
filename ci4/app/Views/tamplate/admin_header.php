73521a5e73b442db9c4fd77e91b393a57149a0b3
Marlina <Marlinahoro63@gmail.com>
2022-06-19 22:36:08 +0700

    menambah file

---
+++ ci4/app/Views/tamplate/admin_header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style1.css">
+    <title>daftar artikel</title>
+</head>
+
+<body>
+
+    <div id="container">
+
+        <header>
+            <h1>daftar artikel</h1>
+        </header>
+        <nav>
+            <a href="<?= base_url('admin/artikel'); ?>" class="active">Dashboard</a>
+            <a href="<?= base_url('/'); ?>">Artikel</a>
+            <a href="<?= base_url('index.php/admin/artikel/add'); ?>">Tambah Artikel</a>
+            <a href="<?= base_url('user/logout'); ?>" class="logout">Logout</a>
+        </nav>
