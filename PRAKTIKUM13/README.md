0f35fa5f987f7ead572623d8263f62c85c1f32e3
Marlina <Marlinahoro63@gmail.com>
2022-06-23 23:46:02 +0700

    menambahkan file

--- PRAKTIKUM13/README.md
+++ PRAKTIKUM13/README.md
@@ -1,1 +1,7 @@
 ## PRAKTIKUM 13
+ ## Langkah-langkah Praktikum
+Persiapan.
+Untuk memulai membuat modul Login, yang perlu disiapkan adalah database server menggunakan MySQL. Pastikan MySQL Server sudah dapat dijalankan melalui XAMPP.
+1. Membuat Tabel: User Login
+
+![11.png](img/1.png)
--- ci4/app/Database/Seeds/UserSeeder.php
+++ ci4/app/Database/Seeds/UserSeeder.php
@@ -8,6 +8,7 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        $model = model('UserModel'); $model->insert([ 'username' => 'admin', 'useremail' => 'admin@email.com', 'userpassword' => password_hash('admin123', PASSWORD_DEFAULT), ]);
+        $model = model('UserModel'); $model->insert([ 'username' => 'admin', 'useremail' => 'admin@email.com', 'userpassword' => 
+        password_hash('admin123', PASSWORD_DEFAULT), ]);
     }
 }
